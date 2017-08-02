e0537444752217cb538033f9874abbd9c70d3358
Alexis Low <alexisthelow@gmail.com>
2017-08-02 14:18:32 -0600

    study drill changes

--- FirstProg.java
+++ FirstProg.java
@@ -1,6 +1,6 @@
 public class FirstProg {
 	public static void main(String[] args) {
-		System.out.println("I am determined to learn how to code.");
-		System.out.println("Today's date is");
+		System.out.println("My name is Alexis Low, and I am determined to learn how to code.");
+		System.out.println("Today's date is 8-2-17.");
 	}
 }
